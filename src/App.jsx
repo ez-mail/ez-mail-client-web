4c6d3229ddf410dd5678af6b3ddaff5e5be59fb4
Jimoon <roadzmoon76@gmail.com>
2023-02-13 11:05:50 +0900

    set: css 초기화 및 기본값 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import GlobalStyles from './GlobalStyles';
 
 function App() {
-  return <div>ez-mail</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>ez-mail</div>
+      <div>또 오셨군요 반가워요</div>
+    </>
+  );
 }
 
 export default App;
