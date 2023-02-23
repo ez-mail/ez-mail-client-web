f2be45b32e5fb213398fe58577b679b31f196554
Jimoon <roadzmoon76@gmail.com>
2023-02-23 17:08:11 +0900

    feat: 이메일 편집 step03 정적인 페이지 추가
    1. 이메일 편집 step03 정적인 페이지를 추가했습니다.

---
+++ src/components/emailEditingStep03/StyleTool.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function StyleTool() {
+  return <div>스타일 툴</div>;
+}
--- src/pages/emails/EmailEditingStep03.jsx
+++ src/pages/emails/EmailEditingStep03.jsx
@@ -1,5 +1,65 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import LeftNav from '../../components/emailEditingStep03/LeftNav';
 
 export default function EmailEditingStep03() {
-  return <div>스텝3</div>;
+  return (
+    <>
+      <SubNav>
+        <Step>구독자</Step>
+        <StyledFaAngleRight icon={faAngleRight} />
+        <Step>발송정보</Step>
+        <StyledFaAngleRight icon={faAngleRight} />
+        <Step>콘텐츠</Step>
+      </SubNav>
+      <Background>
+        <LeftNav />
+        <EmailTemplateContainer>
+          <EmailTemplate />
+        </EmailTemplateContainer>
+      </Background>
+    </>
+  );
 }
+
+const SubNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px 50px;
+  border-bottom: 1px solid #dfe0e4;
+  text-align: center;
+  color: #bdbdbd;
+`;
+
+const Step = styled.span`
+  flex: 2;
+  &:nth-child(5) {
+    font-weight: 500;
+    color: black;
+  }
+`;
+
+const StyledFaAngleRight = styled(FontAwesomeIcon)`
+  font-size: 20px;
+  flex: 0.5;
+`;
+
+const Background = styled.div`
+  display: flex;
+  background-color: #f5f5f5;
+`;
+
+const EmailTemplateContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const EmailTemplate = styled.div`
+  width: 630px;
+  background-color: white;
+`;
