b2c06181e6ca0c5fb18324ba76e2db3e7455c72b
Jimoon <roadzmoon76@gmail.com>
2023-03-06 09:48:55 +0900

    fix: 리액트 쿼리 전역설정에 윈도우 포커스 업데이트 false 설정 추가

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import router from './routes';
 import GlobalStyles from './GlobalStyles';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
